7a6ba08d4e6f3cc683378a47b3e0f9761e5347a6
Roman Oliinyk <oliinyk@oliinyk.local>
2020-09-01 13:40:47 +0300

    redone logic to store operand and operator values to further add into the action log

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 (function createMarkup() {
+  const container = document.createElement('div');
+  container.classList = 'container';
+  document.body.appendChild(container);
+
   const calculator = document.createElement('div');
   calculator.classList = 'calculator';
-  document.body.appendChild(calculator);
+  container.appendChild(calculator);
 
-  const calculatorScreen = document.createElement('input');
-  calculatorScreen.classList = 'calculator-screen';
-  calculatorScreen.type = 'text';
-  calculatorScreen.value = '0';
-  calculatorScreen.disabled = true;
-  calculator.appendChild(calculatorScreen);
+  const displayScreen = document.createElement('div');
+  displayScreen.classList = 'calculator__display';
+  displayScreen.innerText = '0';
+  calculator.appendChild(displayScreen);
 
-  const calculatorKeysContainer = document.createElement('div');
-  calculatorKeysContainer.classList = 'calculator-keys';
-  calculator.appendChild(calculatorKeysContainer);
+  const calculatorKeys = document.createElement('div');
+  calculatorKeys.classList = 'calculator__keys';
+  calculator.appendChild(calculatorKeys);
 
   const operatorsTextArray = ['+', '-', '×', '÷'];
-  const operatorsValuesArray = ['+', '-', '*', '/'];
-  for (let i = 0; i < operatorsValuesArray.length; i++) {
+  const operatorsDataArray = ['add', 'subtract', 'multiply', 'divide'];
+  for (let i = 0; i < operatorsDataArray.length; i++) {
     const calculatorOperators = document.createElement('button');
-    calculatorOperators.classList = 'operator';
-    calculatorOperators.value = operatorsValuesArray[i];
+    calculatorOperators.classList = 'key--operator';
+    calculatorOperators.dataset.action = operatorsDataArray[i];
     calculatorOperators.innerText = operatorsTextArray[i];
-    calculatorKeysContainer.appendChild(calculatorOperators);
+    calculatorKeys.appendChild(calculatorOperators);
   }
 
   const calculatorButtonArray = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
@@ -29,160 +31,197 @@
     const calculatorButton = document.createElement('button');
     calculatorButton.value = calculatorButtonArray[i];
     calculatorButton.innerText = calculatorButtonArray[i];
-    calculatorKeysContainer.appendChild(calculatorButton);
+    calculatorKeys.appendChild(calculatorButton);
   }
 
   const decimalPoint = document.createElement('button');
-  decimalPoint.classList = 'decimal';
-  decimalPoint.value = '.';
+  decimalPoint.dataset.action = 'decimal';
   decimalPoint.innerText = '.'
-  calculatorKeysContainer.appendChild(decimalPoint);
+  calculatorKeys.appendChild(decimalPoint);
 
   const clearButton = document.createElement('button');
-  clearButton.classList = 'all-clear';
-  clearButton.value = 'all-clear';
+  clearButton.dataset.action = 'clear';
   clearButton.innerText = 'AC';
-  calculatorKeysContainer.appendChild(clearButton);
+  calculatorKeys.appendChild(clearButton);
 
   const equalSign = document.createElement('button');
-  equalSign.classList = 'equal-sign operator';
-  equalSign.value = '=';
+  equalSign.classList = 'key--equal';
+  equalSign.dataset.action = 'calculate';
   equalSign.innerText = '=';
-  calculatorKeysContainer.appendChild(equalSign);
+  calculatorKeys.appendChild(equalSign);
 
   const historyContainer = document.createElement('div');
   historyContainer.classList = 'history-container';
-  calculator.appendChild(historyContainer);
+  container.appendChild(historyContainer);
 
   const historyHeading = document.createElement('h1');
-  historyHeading.innerText = 'History Log';
+  historyHeading.innerText = 'Action log';
   historyContainer.appendChild(historyHeading);
 
-  const historyLog = document.createElement('div');
+  const historyLog = document.createElement('ul');
   historyLog.classList = 'history-log';
   historyContainer.appendChild(historyLog);
 })();
 
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', event => {
-  const { target } = event;
-  const { value } = target;
+const historyLog = [];
 
-  if (!target.matches('button')) {
-    return;
+function addToLog(firstOperand, operator, secondOperand, result) {
+  switch (operator) {
+    case 'add':
+      operator = '+';
+      break;
+    case 'subtract':
+      operator = '-';
+      break;
+    case 'multiply':
+      operator = 'x';
+      break;
+    case 'divide':
+      operator = '/';
+      break;
+  }
+  return historyLog.push(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
+}
+
+const calculate = (firstOperand, operator, secondOperand) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(firstOperand) + parseFloat(secondOperand);
+  } else if (operator === 'subtract') {
+    result = parseFloat(firstOperand) - parseFloat(secondOperand);
+  } else if (operator === 'multiply') {
+    result = parseFloat(firstOperand) * parseFloat(secondOperand);
+  } else if (operator === 'divide') {
+    result = parseFloat(firstOperand) / parseFloat(secondOperand);
   }
 
-  switch (value) {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '=':
-      handleOperator(value);
-      break;
-    case '.':
-      inputDecimal(value);
-      break;
-    case 'all-clear':
-      resetCalculator();
-      break;
-    default:
-      if (Number.isInteger(parseFloat(value))) {
-        inputDigit(value);
+  if (result % 1 != 0) {
+    const num = Number(result).toFixed(5);
+    result = num;
+  }
+
+  return result
+}
+
+function updateLog(arr) {
+  const logItem = document.createElement('li');
+  logItem.classList = 'history-log__item';
+  logItem.innerText = arr.map(item => item);
+  document.querySelector('.history-log').appendChild(logItem);
+}
+
+function clearArray(arr) {
+  arr.length = 0;
+}
+
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.calculator__display')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.innerText
+    const displayedNum = display.innerText
+    const previousKeyType = calculator.dataset.previousKeyType
+
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+
+    if (!action) {
+      if (
+        displayedNum === '0' ||
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+      ) {
+        display.innerText = keyContent
+      } else {
+        display.innerText = displayedNum + keyContent
       }
+      calculator.dataset.previousKeyType = 'number'
+    }
+
+    if (action === 'decimal') {
+      if (!displayedNum.includes('.')) {
+        display.innerText = displayedNum + '.'
+      } else if (
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+      ) {
+        display.innerText = '0.'
+      }
+
+      calculator.dataset.previousKeyType = 'decimal'
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      if (
+        firstValue &&
+        operator &&
+        previousKeyType !== 'operator' &&
+        previousKeyType !== 'calculate'
+      ) {
+        const calcValue = calculate(firstValue, operator, secondValue)
+        display.innerText = calcValue
+        calculator.dataset.firstValue = calcValue
+      } else {
+        calculator.dataset.firstValue = displayedNum
+      }
+
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      calculator.dataset.operator = action
+    }
+
+    if (action === 'clear') {
+      if (key.innerText === 'AC') {
+        calculator.dataset.firstValue = ''
+        calculator.dataset.modValue = ''
+        calculator.dataset.operator = ''
+        calculator.dataset.previousKeyType = ''
+      } else {
+        key.innerText = 'AC'
+      }
+
+      display.innerText = 0
+      calculator.dataset.previousKeyType = 'clear'
+    }
+
+    if (action !== 'clear') {
+      const clearButton = calculator.querySelector('[data-action=clear]')
+      clearButton.innerText = 'CE'
+    }
+
+    if (action === 'calculate') {
+      let firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      let secondValue = displayedNum
+
+      if (firstValue) {
+        if (previousKeyType === 'calculate') {
+          firstValue = displayedNum
+          secondValue = calculator.dataset.modValue
+        }
+
+        display.innerText = calculate(firstValue, operator, secondValue)
+      }
+
+      calculator.dataset.modValue = secondValue
+      calculator.dataset.previousKeyType = 'calculate'
+      const result = document.querySelector('.calculator__display').innerHTML;
+      addToLog(firstValue, operator, secondValue, result);
+      updateLog(historyLog);
+      clearArray(historyLog);
+    }
   }
-  updateDisplay();
-});
-
-
-
-const calculator = {
-  displayValue: '0',
-  activeOperand: null,
-  operator: null,
-  waitingForSecondOperand: false
-}
-
-function inputDigit(digit) { 
-  const { displayValue, waitingForSecondOperand} = calculator;
-
-  if (waitingForSecondOperand) { 
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-  }
-
-  console.log(calculator);
-}
-
-function inputDecimal(dot) { 
-  if (calculator.waitingForSecondOperand === true) { 
-    calculator.displayValue = '0.';
-    calculator.waitingForSecondOperand = false;
-    return;
-  }
-
-  if (!calculator.displayValue.includes(dot)) { 
-    calculator.displayValue += dot;
-  }
-}
-
-function handleOperator(nextOperator) { 
-  const { activeOperand, displayValue, operator } = calculator;
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && calculator.waitingForSecondOperand) { 
-    calculator.operator = nextOperator;
-    console.log(calculator);
-    return;
-  }
-
-  if (activeOperand === null && !isNaN(inputValue)) {
-    calculator.activeOperand = inputValue;
-  } else if (operator) { 
-    const result = calculate(activeOperand, inputValue, operator);
-
-    calculator.displayValue = parseFloat(result.toFixed(5));
-    calculator.activeOperand = inputValue;
-  }
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-  // updateLog(calculator);
-  console.log(calculator)
-}
-
-function calculate(firstOperand, secondOperand, operator) {
-  if (operator === '+') {
-    return firstOperand + secondOperand;
-  } else if (operator === '-') {
-    return firstOperand - secondOperand;
-  } else if (operator === '*') {
-    return firstOperand * secondOperand;
-  } else if (operator === '/') {
-    return firstOperand / secondOperand;
-  }
-  return secondOperand;
-}
-
-function resetCalculator() { 
-  calculator.displayValue = '0';
-  calculator.activeOperand = null;
-  calculator.operator = null;
-  calculator.waitingForSecondOperand = false;
-  console.log(calculator);
-}
-
-function updateDisplay() { 
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
-}
-
-// function updateLog(obj) {
-//   const logItem = document.createElement('div');
-//   logItem.classList = 'history-log__item';
-//   logItem.innerText = `${obj.firstOperand} ${obj.operator} ${obj.secondOperand} = ${obj.displayValue}`;
-//   document.querySelector('.history-log').appendChild(logItem);
-// }
-
-updateDisplay();
+})
